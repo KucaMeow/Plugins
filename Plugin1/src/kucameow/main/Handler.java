6403b156568ced22f79d89b2fd239c528c6d2837
Stepan Ponomarev <stepan@ponomarev.pp.ru>
2018-10-20 15:18:31 +0300

    Handler look for you break some blocks

--- Plugin1/src/kucameow/main/Handler.java
+++ Plugin1/src/kucameow/main/Handler.java
@@ -3,6 +3,7 @@ package kucameow.main;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class Handler implements Listener {
@@ -10,5 +11,14 @@ public class Handler implements Listener {
     public void join(PlayerJoinEvent ev){
         Player pl = ev.getPlayer();
         pl.sendMessage("Тебя приветсвует KucaMeow");
+        pl.sendMessage("Получай 10 экспы!");
+        pl.giveExp(10);
+    }
+
+    @EventHandler
+    public void breake(BlockBreakEvent ev){
+        Player p = ev.getPlayer();
+        p.sendMessage("" + p.getDisplayName() + ", ты сломал мой блок! Пожалйста, хватит так делать");
+        p.sendMessage("Это был мой " + ev.getBlock().getType());
     }
 }
