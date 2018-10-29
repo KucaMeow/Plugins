80aa0bc258e83c91936c917ddc6de81fa5a6f991
Stepan Ponomarev <stepan@ponomarev.pp.ru>
2018-10-29 12:18:31 +0300

    Regions copied

--- RegionsCapture/src/kucameow/main/PluginMainClass.java
+++ RegionsCapture/src/kucameow/main/PluginMainClass.java
@@ -1,20 +1,49 @@
 package kucameow.main;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.logging.Logger;
+
+import kucameow.main.commands.RegionCreator;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import kucameow.main.handlers.*;
 //import kucameow.main.commands.*;
 
 public class PluginMainClass extends JavaPlugin{
-    Logger log = Logger.getLogger("Minecraft");
+
+    public File regionsF;
+
+    public Logger log = Logger.getLogger("Minecraft");
+
     public void onEnable() {
+
+        regionsF = new File(getDataFolder() + File.separator + "regions.yml");
+        if(!regionsF.exists()){
+            try {
+                regionsF.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        for (Player p : Bukkit.getOnlinePlayers()){
+            RegionHandler.areas.put(p, new Area(0,0,0,0,0,0));
+        }
+
         log.warning("\n\nInitialisation of RegionCapture plugin started");
-        Bukkit.getPluginManager().registerEvents(new MainHandler(), this);
+        //Initialisation:
+        Bukkit.getPluginManager().registerEvents(new MainHandler(this), this);
+        Bukkit.getPluginManager().registerEvents(new RegionHandler(this), this);
+        getCommand("cr").setExecutor(new RegionCreator(this));
         log.warning("\n\nInitialisation of RegionCapture plugin finished. Plugin works now\n");
     }
     public void onDisable(){
-
+        log.severe("PLUGIN SOMEHOW WAS STOPPED");
+        log.severe("PLUGIN SOMEHOW WAS STOPPED");
+        log.severe("PLUGIN SOMEHOW WAS STOPPED");
+        log.severe("PLUGIN SOMEHOW WAS STOPPED");
+        log.severe("PLUGIN SOMEHOW WAS STOPPED");
     }
 }
--- RegionsCapture/src/kucameow/main/handlers/MainHandler.java
+++ RegionsCapture/src/kucameow/main/handlers/MainHandler.java
@@ -1,6 +1,10 @@
 package kucameow.main.handlers;
 
+import org.apache.logging.log4j.core.config.plugins.Plugin;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.*;
@@ -8,8 +12,24 @@ import org.bukkit.event.block.*;
 import org.bukkit.event.entity.*;
 
 public class MainHandler implements Listener {
+    private kucameow.main.PluginMainClass plugin;
+
+    public MainHandler(kucameow.main.PluginMainClass plugin){
+        this.plugin = plugin;
+    }
+
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event){
-        event.getPlayer().sendMessage(ChatColor.BOLD.DARK_AQUA + "На этом севрере работает плагин захвата территорий. Для справки введите /rc help");
+        if(RegionHandler.areas.get(event.getPlayer()) == null){
+            RegionHandler.areas.put(event.getPlayer(), new Area(0,0,0,0,0,0));
+        }
+        event.getPlayer().sendMessage("" + ChatColor.DARK_AQUA + ChatColor.BOLD + "На этом севрере работает плагин захвата территорий. Для справки введите /rc help");
+    }
+
+    @EventHandler
+    public void onPlayerLeave(PlayerQuitEvent event){
+        if(RegionHandler.areas.get(event.getPlayer()) != null){
+            RegionHandler.areas.remove(event.getPlayer());
+        }
     }
 }
